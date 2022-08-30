9018dd4a487955c7ccd52847fd716581cfc19fbe
Mostepha Amine Bdeira <49349658+MABDesigns@users.noreply.github.com>
2022-08-30 12:07:07 +0100

    updated style.css and finished login page

--- pages/login.php
+++ pages/login.php
@@ -14,7 +14,7 @@
 <body>
 <div class="container-fluid">
     <div class="row d-flex justify-content-center">
-        <div class="col-lg" style="background: white !important; padding: 0 10rem 22rem 10rem !important;">
+        <div class="col-lg mx-auto loginform">
                 <nav class="navbar" id="navbar-login">
                     <a class="navbar-brand" href="#">
                         <img src="../Assets/Logo.svg" alt="logo">
@@ -34,13 +34,26 @@
                     </div>
                     <div class="d-grid">
                     <button type="submit" class="btn btn-login">Login</button>
-                    <div class="text-muted">Not a user yet? <a href="#">Create an account</a></div>
+                    <div class="text-muted">Not a user yet? <a href="register.php">Create an account</a></div>
                     </div>
                 </form>
         </div>
-
         <div class="col-lg">
-
+                <div class="quote-box">
+                    <div class="quote">
+                         <p>"YelpCamp has honestly<br>saved me hours of<br>research time, ad the<br>
+                          camps on here are<br>definitely well picked and<br>added."</p>
+                    </div>
+                    <div class="card border-0 shadow-none bg-transparent">
+                        <div class="card-body d-flex align-items-center p-0">
+                            <img src="..\Assets\User Testimonial.svg" alt="" class="rounded-circle flex-shrink-0 me-3 fit-cover">
+                            <div>
+                                <h5 class="fw-bold text-black mb-0">May Andrews</h5>
+                                <p class="text-muted mb-1 pt-0">Professional Hiker</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
 </div>                
